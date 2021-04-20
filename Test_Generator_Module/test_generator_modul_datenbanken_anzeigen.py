c65ecc7bfac35da30647aeab2b3faed5fc4e4a9e
Genesis <tobias.panteleit@th-koeln.de>
2021-04-20 13:40:30 +0200

    Update test_generator_modul_datenbanken_anzeigen.py
    Auskommentierten Code entfernt

--- Test_Generator_Module/test_generator_modul_datenbanken_anzeigen.py
+++ Test_Generator_Module/test_generator_modul_datenbanken_anzeigen.py
@@ -8,16 +8,6 @@ import sqlite3
 import tkinter as tk
 from tkscrolledframe import ScrolledFrame  #Bewegbares Fesnter (Scrollbalken)
 
-# class _setit:
-#     """Internal class. It wraps the command in the widget OptionMenu."""
-#     def __init__(self, var, value, callback=None):
-#         self.__value = value
-#         self.__var = var
-#         self.__callback = callback
-#     def __call__(self, *args):
-#         self.__var.set(self.__value)
-#         if self.__callback:
-#             self.__callback(self.__value, *args)
 
 class MainGUI:
     def __init__(self, sql_database_name, sql_table_name):
@@ -80,88 +70,3 @@ class MainGUI:
 
         # Alle Spalten-Einträge in Datenbank Übersicht zeigen
         self.tree["displaycolumns"] = df.columns.values.tolist()
-
-
-        # def add_to_header(val):
-        #     new = list(self.tree["displaycolumns"])
-        #     new.append(val)
-        #     self.tree["displaycolumns"] = new
-        #     self.update_option_menu()
-        #
-        #
-        # def rem_from_header(val):
-        #     print(val)
-        #     new = list(self.tree["displaycolumns"])
-        #     new.remove(val)
-        #     self.tree["displaycolumns"] = new
-        #     self.update_option_menu()
-        #
-        # add_var = StringVar(app)
-        # add_var.set(self.tree["columns"])
-        # add = OptionMenu(win, add_var, *self.tree["columns"], command=add_to_header)
-        #
-        # self.rem_var = StringVar(app)
-        # self.rem_var.set(self.tree["displaycolumns"])
-        # self.rem = OptionMenu(win, self.rem_var, *self.tree["displaycolumns"], command=rem_from_header)
-        # Label(win, text="Spalte ausblenden:", font=("bold", 11)).pack()
-        # add.pack(side="top")
-        # Label(win, text="Spalte einblenden:", font=("bold", 11)).pack()
-        # self.rem.pack(side="top")
-
-
-    # def update_option_menu(self):
-    #     def rem_from_header(val):
-    #         print(val)
-    #         new = list(self.tree["displaycolumns"])
-    #         new.remove(val)
-    #         self.tree["displaycolumns"] = new
-    #         self.update_option_menu()
-    #     menu = self.rem["menu"]
-    #     menu.delete(0, "end")
-    #     for string in self.tree["displaycolumns"]:
-    #         menu.add_command(label=string, command=_setit(self.rem_var, string, rem_from_header))
-
-
-    # def onRight(self):
-    #     cursorx = int(self.master.winfo_pointerx() - self.master.winfo_rootx())
-    #     cursory = int(self.master.winfo_pointery() - self.master.winfo_rooty())
-    #     self.menu = Canvas(self.win, width=150, height=40, highlightthickness=1, highlightbackground="black")
-    #     self.menu.place(x=cursorx, y=cursory)
-    #     self.menu.pack_propagate(0)
-    #     delLabel = Label(self.menu, text="  Markierte Zeile löschen", cursor="hand2", anchor="w")
-    #     delLabel.pack(side="top", padx=1, pady=1, fill="x")
-    #     loadLabel = Label(self.menu, text="  Markierte Frage in Tool laden", cursor="hand2", anchor="w")
-    #     loadLabel.pack(side="top", padx=1, pady=1, fill="x")
-    #
-    #
-    #     def destroy():
-    #         self.menu.place_forget()
-    #
-    #     def delete(*args):
-    #         selection = self.tree.selection()
-    #         self.tree.delete(selection)
-    #         destroy()
-    #
-    #     delLabel.bind("<Button-1>", delete)
-    #
-    # def preClick(self, *args):
-    #     try:
-    #         self.menu.place_forget()
-    #         self.onRight()
-    #     except AttributeError:
-    #         self.onRight()
-    #
-    # def onLeft(self, *args):
-    #     self.curItem = self.tree.focus()
-    #     print(self.tree.item(self.curItem)["values"][-1])
-    #     try:
-    #         self.menu.place_forget()
-    #     except AttributeError:
-    #         pass
-    #
-    # def selectItem(self, a):
-    #     curItem = self.tree.focus()
-    #     print(self.tree.item(curItem)["values"][-1])
-
-
-
